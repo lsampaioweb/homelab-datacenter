a2564ecf7c71096b79609d1cb2ccff633b85c80b
Luciano Sampaio <lsampaioweb@gmail.com>
2022-11-16 22:19:18 -0300

    The packer code is finished.

--- pipeline.sh
+++ pipeline.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e # Abort if there is an issue with any build.
 
-cd proxmox-ubuntu-22-04-server-raw/packer/
-./pipeline.sh
-cd ../../
+TIMEFORMAT='Build took %R seconds.'
+time {
 
-cd proxmox-ubuntu-22-04-server-standard/packer/
-./pipeline.sh
-cd ../../
+runningPackerBuild() {
+  echo "Running $1"
+  cd $1
+    ./pipeline.sh
+  cd ../../
+}
 
-cd proxmox-ubuntu-22-04-desktop-raw/packer/
-./pipeline.sh
-cd ../../
+runningPackerBuild "proxmox-ubuntu-22-04-server-raw/packer/"
+runningPackerBuild "proxmox-ubuntu-22-04-server-standard/packer/"
+runningPackerBuild "proxmox-ubuntu-22-04-desktop-raw/packer/"
+runningPackerBuild "proxmox-ubuntu-22-04-desktop-standard/packer/"
 
-cd proxmox-ubuntu-22-04-desktop-standard/packer/
-./pipeline.sh
-cd ../../
+}
