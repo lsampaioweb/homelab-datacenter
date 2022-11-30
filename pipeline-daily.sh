c392b8c02a1ac51fec6798f19e21d3f6544b7aa5
Luciano Sampaio <lsampaioweb@gmail.com>
2022-11-29 21:37:44 -0300

    Added the openssl into the daily script.

--- pipeline-daily.sh
+++ pipeline-daily.sh
@@ -4,17 +4,27 @@ set -e # Abort if there is an issue with any build.
 TIMEFORMAT='It took %R seconds.'
 time {
 
-runJumpServerDailyActions() {
-  echo "Running JumpServer"
-  cd jump_server
+runOpenSSLDailyActions() {
+  echo "Running Open SSL Certificate"
+  cd openssl_certificates
   
   ansible-playbook site.yml
 
   cd -
 }
+
+runJumpServerDailyActions() {
+  echo "Running JumpServer"
+  cd jump-server
+  
+  ansible-playbook site.yml
+
+  cd -
+}
+
 runProxmoxClusterDailyActions() {
   echo "Running Proxmox Cluster"
-  cd proxmox_cluster/
+  cd proxmox-cluster/
   
   ansible-playbook control_machine.yml
   ansible-playbook host_machines.yml
@@ -22,6 +32,7 @@ runProxmoxClusterDailyActions() {
   cd -
 }
 
+runOpenSSLDailyActions
 runJumpServerDailyActions
 runProxmoxClusterDailyActions
 
