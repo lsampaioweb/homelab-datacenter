a9488fc8c95ac9d769193c02fa1b270cc6ad71d8
Luciano Sampaio <lsampaioweb@gmail.com>
2022-12-20 13:02:52 -0300

    Fixed the name of the build.

--- pipeline-template.sh
+++ pipeline-template.sh
@@ -8,12 +8,12 @@ runningPackerBuild() {
   echo "Running $1"
   cd "packer/$1"
     ./pipeline.sh
-  cd ../../
+  cd ../../../
 }
 
-runningPackerBuild "proxmox-ubuntu-22-04-server-raw/packer/"
-runningPackerBuild "proxmox-ubuntu-22-04-server-standard/packer/"
-runningPackerBuild "proxmox-ubuntu-22-04-server-std-docker/packer/"
+# runningPackerBuild "proxmox-ubuntu-22-04-server-raw/packer/"
+# runningPackerBuild "proxmox-ubuntu-22-04-server-standard/packer/"
+# runningPackerBuild "proxmox-ubuntu-22-04-server-std-docker/packer/"
 runningPackerBuild "proxmox-ubuntu-22-04-desktop-raw/packer/"
 runningPackerBuild "proxmox-ubuntu-22-04-desktop-standard/packer/"
 
