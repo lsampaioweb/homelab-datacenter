f3bdf2a218c96ad7b821201b1240143ac8fa7669
Luciano Sampaio <lsampaioweb@gmail.com>
2023-02-21 11:08:59 -0300

    Updated the update ansible submodule bash. Now it pulls, commits and pushes to github.

--- update-ansible-reference.sh
+++ update-ansible-reference.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-set -e # Abort if there is an issue with any build.
+#set -e # Abort if there is an issue when running.
 
 updatingAnsibleSubModule() {
-  echo "Running $1"
+  # Save the current directory.
+  pushd "."
+
+  echo "git pull on $1"
   cd "$1"
-    git pull --recurse-submodules
-  cd -
+  git pull
+
+  echo "git add common"
+  cd ".."
+  git add common
+  git commit -m "Updated ansible common submodule."
+  echo "git push"
+  git push
+
+  # Go back to the original directory.
+  popd
+  echo "Finished."
 }
 
-updatingAnsibleSubModule "../datacenter/ansible/ansible-common-tasks/"
-updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-raw/"
-updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-standard/"
-updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-std-docker/"
-updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-raw/"
-updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-standard/"
-updatingAnsibleSubModule "../datacenter/projects/firewall/"
-updatingAnsibleSubModule "../datacenter/projects/jump-server/"
-updatingAnsibleSubModule "../datacenter/projects/load-balancer/"
-updatingAnsibleSubModule "../datacenter/projects/proxmox-cluster/"
+# updatingAnsibleSubModule "../datacenter/ansible/ansible-common-tasks/"
+updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-raw/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-standard/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-std-docker/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-raw/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-standard/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/projects/dhcp/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/projects/dns/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/projects/firewall/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/projects/jump-server/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/projects/load-balancer/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/projects/ntp/ansible/roles/common/"
+updatingAnsibleSubModule "../datacenter/projects/proxmox-cluster/roles/common/"
