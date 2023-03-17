caa77d4e9bb3b4d536fa0be49f4001137083f0b6
Luciano Sampaio <lsampaioweb@gmail.com>
2023-03-17 11:33:03 -0300

    Added the kvm_setup submodule.

--- update-ansible-reference.sh
+++ update-ansible-reference.sh
@@ -12,28 +12,41 @@ updatingSubModule() {
   cd "$1"
   git pull
 
-  echo "git add common"
+  echo "git add $2"
   cd ".."
-  git add common
-  git commit -m "Updated ansible common submodule."
+  git add $2
+  git commit -m "Updated ansible $2 submodule."
   echo "git push"
   git push
 
   # Go back to the original directory.
   popd
-  echo "Finished."
+  echo -e "Finished.\n"
 }
 
-updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-raw/ansible/roles/common/"
-updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-standard/ansible/roles/common/"
-updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-std-docker/ansible/roles/common/"
-updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-raw/ansible/roles/common/"
-updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-standard/ansible/roles/common/"
-updatingSubModule "../datacenter/projects/dhcp/ansible/roles/common/"
-updatingSubModule "../datacenter/projects/dns/ansible/roles/common/"
-updatingSubModule "../datacenter/projects/firewall/ansible/roles/common/"
-updatingSubModule "../datacenter/projects/jump-server/ansible/roles/common/"
-updatingSubModule "../datacenter/projects/load-balancer/ansible/roles/common/"
-updatingSubModule "../datacenter/projects/ntp/ansible/roles/common/"
-updatingSubModule "../datacenter/projects/openssl-certificates/roles/common/"
-updatingSubModule "../datacenter/projects/proxmox-cluster/roles/common/"
+updatingCommonSubModule() {
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-raw/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-standard/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-std-docker/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-raw/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-standard/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/projects/dhcp/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/projects/dns/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/projects/firewall/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/projects/jump-server/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/projects/load-balancer/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/projects/ntp/ansible/roles/common/" "common"
+  updatingSubModule "../datacenter/projects/openssl-certificates/roles/common/" "common"
+  updatingSubModule "../datacenter/projects/proxmox-cluster/roles/common/" "common"
+}
+
+updatingKvmSetupSubModule() {
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-raw/ansible/roles/kvm_setup/" "kvm_setup"
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-standard/ansible/roles/kvm_setup/" "kvm_setup"
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-server-std-docker/ansible/roles/kvm_setup/" "kvm_setup"
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-raw/ansible/roles/kvm_setup/" "kvm_setup"
+  updatingSubModule "../datacenter/packer/proxmox-ubuntu-22-04-desktop-standard/ansible/roles/kvm_setup/" "kvm_setup"
+}
+
+updatingCommonSubModule
+updatingKvmSetupSubModule
