ee97fde7b5d7f06818e54ea653044aeb5513ffd9
Luciano Sampaio <lsampaioweb@gmail.com>
2024-04-14 08:29:26 -0300

    chore: Update Packer Template IDs to reflect new numbering scheme.

--- templates.sh
+++ templates.sh
@@ -2,9 +2,9 @@
 set -e # Abort if there is an issue with any build.
 
 # Usage:
-#   ./templates.sh validate staging
+#   ./templates.sh validate home
 #   $1 -> validate, build.
-#   $2 -> staging, production.
+#   $2 -> home, homelab.
 
 runningPackerBuild() {
   TIMEFORMAT='Build took %R seconds.'
@@ -19,19 +19,19 @@ runningPackerBuild() {
   echo -e "\n"
 }
 
-# runningPackerBuild "proxmox-firewall-server-raw/packer/" $1 $2
 runningPackerBuild "proxmox-ubuntu-22-04-server-raw/packer/" $1 $2
 runningPackerBuild "proxmox-ubuntu-22-04-server-standard/packer/" $1 $2
 runningPackerBuild "proxmox-ubuntu-22-04-server-std-docker/packer/" $1 $2
+runningPackerBuild "proxmox-ubuntu-22-04-server-std-k3s/packer/" $1 $2
 runningPackerBuild "proxmox-ubuntu-22-04-desktop-raw/packer/" $1 $2
 runningPackerBuild "proxmox-ubuntu-22-04-desktop-standard/packer/" $1 $2
 
 # Run these commands on the node that has the template files.
 : << 'MULTILINE-COMMENT'
-qm destroy 905 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 904 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 903 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 902 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 901 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 900 --purge 1 --destroy-unreferenced-disks 1
+qm destroy 931 --purge 1 --destroy-unreferenced-disks 1; \
+qm destroy 921 --purge 1 --destroy-unreferenced-disks 1; \
+qm destroy 913 --purge 1 --destroy-unreferenced-disks 1; \
+qm destroy 912 --purge 1 --destroy-unreferenced-disks 1; \
+qm destroy 911 --purge 1 --destroy-unreferenced-disks 1; \
+qm destroy 901 --purge 1 --destroy-unreferenced-disks 1
 MULTILINE-COMMENT
