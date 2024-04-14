4d90a4d5afd5b33ecebacf73252e947b102d2cb5
Luciano Sampaio <lsampaioweb@gmail.com>
2024-04-14 08:36:50 -0300

    chore: create a bash function that will create all Packer templates.

--- templates.sh
+++ templates.sh
@@ -16,22 +16,28 @@ runningPackerBuild() {
     echo "Finished $1"
   }
 
-  echo -e "\n"
+  echo ""
 }
 
-runningPackerBuild "proxmox-ubuntu-22-04-server-raw/packer/" $1 $2
-runningPackerBuild "proxmox-ubuntu-22-04-server-standard/packer/" $1 $2
-runningPackerBuild "proxmox-ubuntu-22-04-server-std-docker/packer/" $1 $2
-runningPackerBuild "proxmox-ubuntu-22-04-server-std-k3s/packer/" $1 $2
-runningPackerBuild "proxmox-ubuntu-22-04-desktop-raw/packer/" $1 $2
-runningPackerBuild "proxmox-ubuntu-22-04-desktop-standard/packer/" $1 $2
+function createTemplates() {
+  runningPackerBuild "proxmox-ubuntu-22-04-server-raw/packer/" $1 $2
 
-# Run these commands on the node that has the template files.
-: << 'MULTILINE-COMMENT'
-qm destroy 931 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 921 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 913 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 912 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 911 --purge 1 --destroy-unreferenced-disks 1; \
-qm destroy 901 --purge 1 --destroy-unreferenced-disks 1
-MULTILINE-COMMENT
+  runningPackerBuild "proxmox-ubuntu-22-04-server-standard/packer/" $1 $2
+  runningPackerBuild "proxmox-ubuntu-22-04-server-std-docker/packer/" $1 $2
+  runningPackerBuild "proxmox-ubuntu-22-04-server-std-k3s/packer/" $1 $2
+
+  runningPackerBuild "proxmox-ubuntu-22-04-desktop-raw/packer/" $1 $2
+  runningPackerBuild "proxmox-ubuntu-22-04-desktop-standard/packer/" $1 $2
+}
+
+createTemplates
+
+# # Run these commands on the node that has the template files.
+# : << 'MULTILINE-COMMENT'
+# qm destroy 931 --purge 1 --destroy-unreferenced-disks 1; \
+# qm destroy 921 --purge 1 --destroy-unreferenced-disks 1; \
+# qm destroy 913 --purge 1 --destroy-unreferenced-disks 1; \
+# qm destroy 912 --purge 1 --destroy-unreferenced-disks 1; \
+# qm destroy 911 --purge 1 --destroy-unreferenced-disks 1; \
+# qm destroy 901 --purge 1 --destroy-unreferenced-disks 1
+# MULTILINE-COMMENT
