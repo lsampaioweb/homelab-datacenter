0bafa3e8c70b939ae3f825a48ce4c1fb9fa2ba57
Luciano Sampaio Martins de Souza <lsampaioweb@gmail.com>
2025-10-04 08:11:13 -0300

    chore: Refactor JumpServer update function placement in daily updates script.

--- scripts/projects/04-ansible/run-daily-updates.sh
+++ scripts/projects/04-ansible/run-daily-updates.sh
@@ -17,14 +17,6 @@ create_directory "$LOG_DIR"
 exec 1> >(tee -a "$LOG_FILE")
 exec 2>&1
 
-run_jumpserver_daily_updates() {
-  log_info "Running JumpServer updates."
-
-  navigate_to_dir "$HOME/git/datacenter/06-projects/01-working-machine/ansible"
-  ansible-playbook update.yml
-  return_to_previous_dir
-}
-
 run_openssl_daily_updates() {
   log_info "Running OpenSSL certificate updates."
 
@@ -41,8 +33,16 @@ run_proxmox_cluster_daily_updates() {
   return_to_previous_dir
 }
 
-measure_time "JumpServer updates" run_jumpserver_daily_updates
+run_jumpserver_daily_updates() {
+  log_info "Running JumpServer updates."
+
+  navigate_to_dir "$HOME/git/datacenter/06-projects/01-working-machine/ansible"
+  ansible-playbook update.yml
+  return_to_previous_dir
+}
+
 measure_time "OpenSSL certificate updates" run_openssl_daily_updates
 measure_time "Proxmox cluster updates" run_proxmox_cluster_daily_updates
+measure_time "JumpServer updates" run_jumpserver_daily_updates
 
 log_info "Daily updates completed successfully."
